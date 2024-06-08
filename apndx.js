198337be8c06891926c1bc299be029fec7ea0b48
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2024-06-08 18:15:49 +0300

    Use scrollable background

--- apndx.js
+++ apndx.js
@@ -6,6 +6,10 @@ function draw() {
 
     textFont('Press Start 2P')
 
+    if (demoTime < 64) {
+      scrollBg(demoTime)
+    }
+
     if (demoTime < 12) {
       title(demoTime)
 
@@ -32,6 +36,23 @@ function draw() {
  
     }
 }
+
+
+function scrollBg(sceneTime){
+  image(img, 0, bgY1, width, height);
+  image(img, 0, bgY2, width, height);
+
+  bgY1 += bgScrollSpeed;
+  bgY2 += bgScrollSpeed;
+
+  if (bgY1 > height){
+    bgY1 = -height;
+  }
+  if (bgY2 > height){
+    bgY2 = -height;
+  }
+}
+
 
 
 function title(sceneTime) {
@@ -76,108 +97,20 @@ function intros(sceneTime) {
 }
 
 function particlator(sceneTime) {
-// an array to add multiple particles
-let particles = [];
+  //background(img);
+  // an array to add multiple particles
+  let particles = [];
 
-  for(let i = 0;i<width/1000;i++){
-    particles.push(new Particle());
-  }
-
-  for(let i = 0;i<particles.length;i++) {
-    particles[i].createParticle();
-    particles[i].moveParticle();
-    particles[i].joinParticles(particles.slice(i));
-  }
-}
-
-function instructionsScene(sceneTime) {
-    background(12, 14, 26, 255)
-    textAlign(CENTER, CENTER)
-    textSize(64)
-    fill(35, 100, 50);
-    noStroke()
-    text('Press F to go fullscreen and space to start', width/2, height/2)
-}
-
-
-function stickScene(sceneTime) {
-    background(35, 100, 50) // orange
-
-    translate(width/2, height/2)
-
-    fill(0, 0, 0) // black
-    noStroke()
-    // https://www.w3schools.com/jsref/jsref_tofixed.asp
-    text(sceneTime.toFixed(2), 0, 0)
-
-    stroke(0, 0, 0) // black
-    strokeWeight(10)
-
-    // https://p5js.org/reference/#/p5/rotate
-    rotate(sceneTime)
-    line(0, 100, 0, 300)
-}
-
-
-
-function ballScene(sceneTime) {
-    background(210, 100, 50) // blue
-
-    translate(width/2, height/2)
-
-    fill(0, 0, 100) // white
-    noStroke()
-
-    text(sceneTime.toFixed(2), 0, 0)
-
-    rotate(sceneTime)
-
-    const bump = sceneTime - Math.floor(sceneTime)
-    // https://p5js.org/reference/#/p5/map
-    const radius = map(bump, 0, 1, 0, 100)
-    ellipse(0, 200, radius, radius)
-}
-
-
-
-function squareScene(sceneTime) {
-    background(340, 100, 50) // purple
-
-    translate(width/2, height/2)
-
-    stroke(0, 0, 100) // white
-    strokeWeight(10)
-    noFill()
-
-    // https://p5js.org/reference/#/p5/rectMode
-    rectMode(CENTER)
-    for (let i = 0; i < 10; i++) {
-        rotate(sceneTime * 0.1)
-        // https://p5js.org/reference/#/p5/scale
-        scale(0.9)
-        rect(0, 0, 500, 500)
+    for(let i = 0;i<width/1000;i++){
+      particles.push(new Particle());
     }
-}
 
-
-
-function noiseScene(sceneTime) {
-    background(130, 50, 50) // green
-
-    fill(255)
-    noStroke()
-
-    // https://p5js.org/reference/#/p5/beginShape
-    beginShape()
-    for (let i = 0; i < 100; i++) {
-        const x = map(i, 0, 99, 0, width)
-        // https://p5js.org/reference/#/p5/noise
-        const y = noise(i * 0.1, sceneTime / 16) * height
-        vertex(x, y)
+    for(let i = 0;i<particles.length;i++) {
+      particles[i].createParticle();
+      particles[i].moveParticle();
+      particles[i].joinParticles(particles.slice(i));
     }
-    vertex(width, 800)
-    vertex(0, 800)
-    endShape(CLOSE)
+    
 }
 
 
