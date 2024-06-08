f31fbf3cf20c9d16621c11113c8553bd8a18422e
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2024-06-08 17:27:31 +0300

    Add particle lasers

--- apndx.js
+++ apndx.js
@@ -12,11 +12,8 @@ function draw() {
     } else if (demoTime < 28) {
       intros(demoTime-12)
 
-    } else if (demoTime < 30) {
-        //stickScene(demoTime - 16)
-
-    } else if (demoTime < 32) {
-        //ballScene(demoTime)
+    } else if (demoTime < 40) {
+      particlator(demoTime - 28)
 
     } else if (demoTime < 40) {
         //squareScene(demoTime - 32)
@@ -63,7 +60,7 @@ function intros(sceneTime) {
 
 
 
-    
+
 
     Credits
     hajame viku apndx
@@ -76,6 +73,21 @@ function intros(sceneTime) {
     `;
 
   text(texts, width/2, y)
+}
+
+function particlator(sceneTime) {
+// an array to add multiple particles
+let particles = [];
+
+  for(let i = 0;i<width/1000;i++){
+    particles.push(new Particle());
+  }
+
+  for(let i = 0;i<particles.length;i++) {
+    particles[i].createParticle();
+    particles[i].moveParticle();
+    particles[i].joinParticles(particles.slice(i));
+  }
 }
 
 function instructionsScene(sceneTime) {
@@ -166,4 +178,46 @@ function noiseScene(sceneTime) {
     vertex(width, 800)
     vertex(0, 800)
     endShape(CLOSE)
+}
+
+
+class Particle {
+  // setting the co-ordinates, radius and the
+  // speed of a particle in both the co-ordinates axes.
+  constructor(){
+    this.x = random(0,width);
+    this.y = random(0,height);
+    this.r = random(1,8);
+    this.xSpeed = random(-2,2);
+    this.ySpeed = random(-1,1.5);
+  }
+
+// creation of a particle.
+  createParticle() {
+    noStroke();
+    fill('rgba(200,169,169,0.5)');
+    circle(this.x,this.y,this.r);
+  }
+
+// setting the particle in motion.
+  moveParticle() {
+    if(this.x < 0 || this.x > width)
+      this.xSpeed*=-1;
+    if(this.y < 0 || this.y > height)
+      this.ySpeed*=-1;
+    this.x+=this.xSpeed;
+    this.y+=this.ySpeed;
+  }
+
+// this function creates the connections(lines)
+// between particles which are less than a certain distance apart
+  joinParticles(particles) {
+    particles.forEach(element =>{
+      let dis = dist(this.x,this.y,element.x,element.y);
+      if(dis<500) {
+        stroke('rgba(35, 100, 50,255)');
+        line(this.x,this.y,element.x,element.y);
+      }
+    });
+  }
 }
